a411dbc1bca5d9e4c6d384a6dc5acca6f9b2b44e
hassanShakur <hass.shakurhassan@gmail.com>
2023-12-23 21:32:56 +0300

    create the search js logic

---
+++ app/assets/javascript/search.js
@@ -0,0 +1,38 @@
+// app/assets/javascripts/articles.js
+document.addEventListener('DOMContentLoaded', function () {
+  var typingTimer;
+
+  // Function to execute when typing is done
+  function doneTyping() {
+    var searchTerm = document.getElementById('search_term').value;
+    console.log('Search term:', searchTerm);
+    // Make an AJAX request to update search results
+    updateSearchResults(searchTerm);
+  }
+
+  // Function to make an AJAX request and update search results
+  function updateSearchResults(searchTerm) {
+    $.ajax({
+      url: '/articles',
+      type: 'GET',
+      data: { term: searchTerm },
+      dataType: 'script', // Expecting JavaScript response
+      success: function (response) {
+        // Handle success if needed
+        console.log(response);
+        $('#articles').html(response);
+      },
+      error: function (error) {
+        console.error('Error:', error.responseText);
+      },
+    });
+  }
+
+  // Listen for input events on the search term field
+  document
+    .getElementById('search_term')
+    .addEventListener('input', function () {
+      clearTimeout(typingTimer);
+      typingTimer = setTimeout(doneTyping, 500);
+    });
+});
