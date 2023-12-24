b49c11d48587d6efb1aa4f71086f5bb94ea7087b
hassanShakur <hass.shakurhassan@gmail.com>
2023-12-24 12:52:33 +0300

    set article user fixing article creation bug ✅

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,6 +43,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = current_user
 
     if @article.save
       redirect_to @article, notice: 'Article was successfully created.'
