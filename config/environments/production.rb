9c3e440caa8770a677b0537547d935d085f0ebc5
hassanShakur <hass.shakurhassan@gmail.com>
2023-12-24 09:45:15 +0300

    test file updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
